8922b085c78e95f70e1367e6a7cae08a6834861c
Foursqweez <jeerasak.code@gmail.com>
2025-02-04 17:39:55 +0700

    Spacebar to increment

--- fancy-counter/src/components/Card.jsx
+++ fancy-counter/src/components/Card.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ButtonContainer from "./ButtonContainer"
 import Count from "./Count"
 import ResetButton from "./ResetButton"
@@ -7,6 +7,26 @@ import Title from "./Title"
 export default function Card() {
   const [count, setCount] = useState(0)
   const locked = count === 5 ? true : false
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      if (event.code === "Space") {
+        const newCount = count + 1
+        if (newCount > 5) {
+          setCount(5)
+          return
+        }
+        setCount(newCount)
+      }
+    }
+
+    window.addEventListener("keydown", handleKeydown)
+
+    // cleanup function: clean the old event first before add it again to prevent the app slowdown because it has too may events
+    return () => {
+      window.removeEventListener("keydown", handleKeydown)
+    }
+  }, [count])
 
   return (
     <div className={`card ${locked ? "card--limit" : ""}`}>
