cd9a4f2942070f7b599779ae0351eb3d78ea4fce
Foursqweez <jeerasak.code@gmail.com>
2025-02-04 19:54:15 +0700

    Handle Events (Change Event)

--- word-anlytics/src/components/Textarea.jsx
+++ word-anlytics/src/components/Textarea.jsx
@@ -1,3 +1,13 @@
 export default function Textarea() {
-  return <textarea className="textarea" />
+  return (
+    <textarea
+      onChange={(e) => {
+        console.log("change event...")
+        console.log(e.target.value)
+      }}
+      className="textarea"
+      placeholder="Enter your text"
+      spellCheck={false}
+    />
+  )
 }
