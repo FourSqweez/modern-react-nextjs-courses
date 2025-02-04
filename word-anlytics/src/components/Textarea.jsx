d7e9498a934176231058b3db0188627ce2c81abd
Foursqweez <jeerasak.code@gmail.com>
2025-02-04 20:16:00 +0700

    Basic validation

--- word-anlytics/src/components/Textarea.jsx
+++ word-anlytics/src/components/Textarea.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react"
+import Warning from "./Warning"
 
 export default function Textarea() {
   const [text, setText] = useState("")
+  const [showWarning, setShowWarning] = useState(false)
+
+  const handleChange = (e) => {
+    let newText = e.target.value
+    if (newText.includes("<script>")) {
+      setShowWarning(true)
+      newText = newText.replace("<script>", "")
+    }
+    setText(newText)
+  }
 
   return (
-    <textarea
-      value={text}
-      onChange={(e) => {
-        setText(e.target.value)
-      }}
-      className="textarea"
-      placeholder="Enter your text"
-      spellCheck={false}
-    />
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck={false}
+      />
+      {showWarning ? <Warning /> : null}
+    </div>
   )
 }
