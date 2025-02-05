7d38ae46f5b4d18144300c95af024184691ab1fa
Foursqweez <jeerasak.code@gmail.com>
2025-02-05 14:39:26 +0700

    AddItemForm, Spread operator, Drilling prop, Constants

--- trekbag/src/components/AddItemForm.jsx
+++ trekbag/src/components/AddItemForm.jsx
@@ -1,10 +1,38 @@
+import { useRef, useState } from "react"
 import Button from "./Button"
 
-export default function AddItemForm() {
+export default function AddItemForm({ setItems }) {
+  const [itemText, setItemText] = useState("")
+  const inputRef = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // basic validation
+    if (!itemText) {
+      alert("Item can't be empty")
+      inputRef.current.focus()
+      return
+    }
+
+    const newItem = {
+      id: new Date().getTime(),
+      name: itemText,
+      packed: false,
+    }
+    setItems((prev) => [...prev, newItem])
+    setItemText("")
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Add an Item</h2>
-      <input />
+      <input
+        ref={inputRef}
+        value={itemText}
+        onChange={(e) => setItemText(e.target.value)}
+        autoFocus
+      />
       <Button>Add to list</Button>
     </form>
   )
