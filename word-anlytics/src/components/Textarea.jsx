c7f2878cf56e06f7d0d6f824159803092685b1c3
Foursqweez <jeerasak.code@gmail.com>
2025-02-05 10:03:57 +0700

    Derived State (numberOfCharacters)

--- word-anlytics/src/components/Textarea.jsx
+++ word-anlytics/src/components/Textarea.jsx
@@ -5,6 +5,8 @@ export default function Textarea() {
   const [text, setText] = useState("")
   const [warningText, setWarningText] = useState("")
 
+  const numberOfCharacters = text.length
+
   const handleChange = (e) => {
     let newText = e.target.value
 
