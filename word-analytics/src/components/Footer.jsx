6205adffff273b32298986c9d7c26f30e9a11ab0
Foursqweez <jeerasak.code@gmail.com>
2025-02-05 20:53:27 +0700

    Fix word spelling

--- word-analytics/src/components/Footer.jsx
+++ word-analytics/src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <small>&copy; Copyright by Four. All rights reserved.</small>
-      <small>Last checked limits: 17 dys ago</small>
+      <small>Last checked limits: 17 days ago</small>
     </footer>
   )
 }
