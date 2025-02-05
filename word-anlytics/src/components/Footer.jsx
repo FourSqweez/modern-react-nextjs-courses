7edc89ebafdcb3af6e28bb506fff89e566567619
Foursqweez <jeerasak.code@gmail.com>
2025-02-05 10:37:44 +0700

    Update folder structure

--- word-anlytics/src/components/Footer.jsx
+++ word-anlytics/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <small>&copy; Copyright by BytGrad. All rights reserved.</small>
+      <small>&copy; Copyright by Four. All rights reserved.</small>
       <small>Last checked limits: 17 dys ago</small>
     </footer>
   )
