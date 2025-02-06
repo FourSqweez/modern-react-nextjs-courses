6c50cbaa29a4dca0efaf261a88a4eff97267035b
Foursqweez <jeerasak.code@gmail.com>
2025-02-06 10:49:39 +0700

    Count the max character with useState, controlled input, derived state

--- corpcomment/src/components/FeedbackForm.tsx
+++ corpcomment/src/components/FeedbackForm.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react"
+import { MAX_CHARACTERS } from "../lib/constants"
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("")
+
+  const charCount = MAX_CHARACTERS - text.length
+
   return (
     <form className="form">
       <textarea
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value)
+        }}
         name=""
         id="feedback-textarea"
         placeholder=""
@@ -13,7 +24,7 @@ export default function FeedbackForm() {
       </label>
 
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
