cbba6f022f6cfa3fc1b0060887e241ec243ae95b
Foursqweez <jeerasak.code@gmail.com>
2025-02-06 10:38:08 +0700

    Setup components, style, icons

---
+++ corpcomment/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          &copy; 2025. Copyright by Four. Inspired by{" "}
+          <a target="_blank" href="https://bytegrad.com">
+            ByteGrad’s.com
+          </a>{" "}
+          course.
+        </p>
+        <p>
+          <span className="u-bold u-italic">Not allowed</span> to use as your
+          own teaching.
+        </p>
+      </small>
+    </footer>
+  )
+}
---
+++ corpcomment/src/components/HashtagList.tsx
@@ -0,0 +1,21 @@
+export default function HashtagList() {
+  return (
+    <ul className="hashtags">
+      <li>
+        <button>#ByteGrad</button>
+      </li>
+      <li>
+        <button>#ByteGrad</button>
+      </li>
+      <li>
+        <button>#ByteGrad</button>
+      </li>
+      <li>
+        <button>#ByteGrad</button>
+      </li>
+      <li>
+        <button>#ByteGrad</button>
+      </li>
+    </ul>
+  )
+}
