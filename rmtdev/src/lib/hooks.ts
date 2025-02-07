b7589e3b19d0a02021caeefa040e93fde564122a
Foursqweez <jeerasak.code@gmail.com>
2025-02-07 14:19:12 +0700

    Custom hook for ActiveId (using hashchange eventListener)

--- rmtdev/src/lib/hooks.ts
+++ rmtdev/src/lib/hooks.ts
@@ -25,3 +25,22 @@ export function useJobItems(searchText: string) {
 
   return [jobItemsSliced, isLoading] as const;
 }
+
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      const id = +window.location.hash.slice(1);
+      setActiveId(id);
+    };
+    handleHashChange();
+    window.addEventListener('hashchange', handleHashChange);
+
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  }, []);
+
+  return activeId;
+}
