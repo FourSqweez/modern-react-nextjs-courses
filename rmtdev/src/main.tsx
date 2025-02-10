2f1b4f6a540dc3d73375e83fecd94dd30b6244c7
Foursqweez <jeerasak.code@gmail.com>
2025-02-10 19:03:02 +0700

    Uncomment strick mode

--- rmtdev/src/main.tsx
+++ rmtdev/src/main.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.tsx';
 import ActiveIdContextProvider from './contexts/ActiveIdContextProvider.tsx';
@@ -10,17 +11,17 @@ import './index.css';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <BookmarksContextProvider>
-      <ActiveIdContextProvider>
-        <SearchTextContextProvider>
-          <JobItemsContextProvider>
-            <App />
-          </JobItemsContextProvider>
-        </SearchTextContextProvider>
-      </ActiveIdContextProvider>
-    </BookmarksContextProvider>
-  </QueryClientProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BookmarksContextProvider>
+        <ActiveIdContextProvider>
+          <SearchTextContextProvider>
+            <JobItemsContextProvider>
+              <App />
+            </JobItemsContextProvider>
+          </SearchTextContextProvider>
+        </ActiveIdContextProvider>
+      </BookmarksContextProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
