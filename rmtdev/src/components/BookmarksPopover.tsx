251519bc53885f8a3ea634507d41ad421f08d544
Foursqweez <jeerasak.code@gmail.com>
2025-02-10 16:01:16 +0700

    Create Portal for Popover to be in the body instead of in header level

--- rmtdev/src/components/BookmarksPopover.tsx
+++ rmtdev/src/components/BookmarksPopover.tsx
@@ -1,13 +1,15 @@
 import { forwardRef } from 'react';
+import { createPortal } from 'react-dom';
 import { useBookMarksContext } from '../lib/hooks';
 import JobList from './JobList';
 
 const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useBookMarksContext();
-  return (
+  return createPortal(
     <div ref={ref} className="bookmarks-popover">
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
-    </div>
+    </div>,
+    document.body
   );
 });
 
