cd7d22965e79b95252b073980578a58c912fa841
Foursqweez <jeerasak.code@gmail.com>
2025-02-10 18:16:20 +0700

    UseMemo (Memoize derived state) - prevent more calculate when rerender

--- rmtdev/src/contexts/JobItemsContextProvider.tsx
+++ rmtdev/src/contexts/JobItemsContextProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useMemo, useState } from 'react';
 import { RESULTS_PER_PAGE } from '../lib/constants';
 import { useSearchQuery, useSearchTextContext } from '../lib/hooks';
 import { JobItem, PageDirection, SortBy } from '../lib/types';
@@ -8,7 +8,6 @@ type JobItemsContextProviderProps = {
 };
 
 type TJobItemsContext = {
-  jobItems: JobItem[] | undefined;
   isLoading: boolean;
   jobItemsSortedAndSliced: JobItem[];
   sortBy: SortBy;
@@ -32,21 +31,31 @@ export default function JobItemsContextProvider({
   // derived / computed state
   const totalNumberOfResults = jobItems?.length || 0;
   const totalNumberOfPages = totalNumberOfResults / RESULTS_PER_PAGE;
-  const jobItemsSorted = [...(jobItems || [])].sort((a, b) => {
-    if (sortBy === 'relevant') {
-      return b.relevanceScore - a.relevanceScore;
-    } else {
-      return a.daysAgo - b.daysAgo;
-    }
-  });
 
-  const jobItemsSortedAndSliced = jobItemsSorted.slice(
-    currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
-    currentPage * RESULTS_PER_PAGE
+  // Sorting is heavy job for website so it's not good to do it many times. so prevent that with useMemo
+  const jobItemsSorted = useMemo(
+    () =>
+      [...(jobItems || [])].sort((a, b) => {
+        if (sortBy === 'relevant') {
+          return b.relevanceScore - a.relevanceScore;
+        } else {
+          return a.daysAgo - b.daysAgo;
+        }
+      }),
+    [sortBy, jobItems]
+  );
+
+  // Sorting should done first before do this
+  const jobItemsSortedAndSliced = useMemo(
+    () =>
+      jobItemsSorted.slice(
+        currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
+        currentPage * RESULTS_PER_PAGE
+      ),
+    [currentPage, jobItemsSorted]
   );
 
   // event handlers / actions
-
   const handleChangePage = (direction: PageDirection) => {
     if (direction === 'next') {
       setCurrentPage((prev) => prev + 1);
@@ -63,7 +72,6 @@ export default function JobItemsContextProvider({
   return (
     <JobItemsContext.Provider
       value={{
-        jobItems,
         isLoading,
         jobItemsSortedAndSliced,
         currentPage,
