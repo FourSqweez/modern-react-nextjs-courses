8f50d1c2cbc3bc18ac01da23d79aa76dc224ddf6
Foursqweez <jeerasak.code@gmail.com>
2025-02-11 17:17:40 +0700

    Events page (Scrollbar styling)

--- evento/src/app/events/[city]/page.tsx
+++ evento/src/app/events/[city]/page.tsx
@@ -2,7 +2,7 @@ import H1 from '@/components/H1'
 
 export default function EventsPage() {
   return (
-    <main>
+    <main className="flex min-h-[110vh] flex-col items-center px-[20px] py-24">
       <H1>Events in Austin</H1>
     </main>
   )
