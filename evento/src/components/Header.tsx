441f24249fa1fae764b32f126f78bccb997fc200
Foursqweez <jeerasak.code@gmail.com>
2025-02-11 16:31:19 +0700

    Active link (clsx, framer motion)

--- evento/src/components/Header.tsx
+++ evento/src/components/Header.tsx
@@ -1,4 +1,8 @@
+'use client'
+import clsx from 'clsx'
+import { motion } from 'framer-motion'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import Logo from './Logo'
 
 const links = [
@@ -11,18 +15,32 @@ const links = [
     path: '/events/all',
   },
 ]
+
 export default function Header() {
+  const activePathname = usePathname()
   return (
     <header className="flex h-14 items-center justify-between border-b border-white/10 px-3 sm:px-9">
       <Logo />
-      <nav>
-        <ul className="flex gap-x-6">
+      <nav className="h-full">
+        <ul className="flex h-full gap-x-6">
           {links.map((route) => (
             <li
               key={route.path}
-              className="text-white/50 transition hover:text-white"
+              className={clsx(
+                'relative flex items-center transition hover:text-white',
+                {
+                  'text-white': activePathname === route.path,
+                  'text-white/50': activePathname !== route.path,
+                },
+              )}
             >
               <Link href={route.path}>{route.name}</Link>
+              {activePathname === route.path && (
+                <motion.div
+                  layoutId="header-active-link"
+                  className="bg-accent absolute bottom-0 h-1 w-full"
+                />
+              )}
             </li>
           ))}
         </ul>
