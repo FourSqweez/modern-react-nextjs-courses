b4d7c103c15bcfa83dce13e3f0620f2ace7e678b
Foursqweez <jeerasak.code@gmail.com>
2025-02-11 18:03:21 +0700

    EventCard component (Flexbox tricks) - flex-1, basis, wrap

--- evento/src/components/EventsList.tsx
+++ evento/src/components/EventsList.tsx
@@ -7,7 +7,7 @@ type EventsListProps = {
 
 export default function EventsList({ events }: EventsListProps) {
   return (
-    <section>
+    <section className="flex max-w-[1100px] flex-wrap justify-center gap-10 px-[20px]">
       {events.map((event) => (
         <EventCard key={event.id} event={event} />
       ))}
