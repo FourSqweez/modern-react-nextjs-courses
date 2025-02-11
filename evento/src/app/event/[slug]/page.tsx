d5345643a3d46e86d87aea6d5063420df2114372
Foursqweez <jeerasak.code@gmail.com>
2025-02-11 20:01:17 +0700

    Fetch Event from url using slug params

--- evento/src/app/event/[slug]/page.tsx
+++ evento/src/app/event/[slug]/page.tsx
@@ -1,3 +1,15 @@
-export default function EventPage() {
+type EventPageProps = {
+  params: {
+    slug: string
+  }
+}
+
+export default async function EventPage({ params }: EventPageProps) {
+  const { slug } = params
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`,
+  )
+  const event = await response.json()
+  console.log('Event :', event)
   return <div>EventPage</div>
 }
