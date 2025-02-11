5db8f275cb81af02bc48a88b50a846649aefd72d
Foursqweez <jeerasak.code@gmail.com>
2025-02-11 15:50:03 +0700

    Header styling

--- evento/src/components/Header.tsx
+++ evento/src/components/Header.tsx
@@ -1,12 +1,32 @@
 import Link from 'next/link'
 import Logo from './Logo'
 
+const links = [
+  {
+    name: 'Home',
+    path: '/',
+  },
+  {
+    name: 'All Events',
+    path: '/events/all',
+  },
+]
 export default function Header() {
   return (
-    <header>
+    <header className="flex h-14 items-center justify-between border-b border-white/10 px-3 sm:px-9">
       <Logo />
-      <Link href="/">Home</Link>
-      <Link href="/events/all">All Events</Link>
+      <nav>
+        <ul className="flex gap-x-6">
+          {links.map((route) => (
+            <li
+              key={route.path}
+              className="text-white/50 transition hover:text-white"
+            >
+              <Link href={route.path}>{route.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   )
 }
