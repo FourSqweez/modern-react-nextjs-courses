f3b40065913bbe633ddfde48228f575fc266cd18
Foursqweez <jeerasak.code@gmail.com>
2025-02-12 13:36:45 +0700

    SkeletonCard component

--- evento/src/app/events/[city]/loading.tsx
+++ evento/src/app/events/[city]/loading.tsx
@@ -1,3 +1,14 @@
+import SkeletonCard from '@/components/SkeletonCard'
+
 export default function Loading() {
-  return <div>Loading events...</div>
+  return (
+    <div className="mx-auto flex max-w-[1100px] flex-wrap justify-center gap-20 px-[20px] py-24">
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      {Array.from({ length: 6 }).map((item, i) => (
+        <SkeletonCard key={i} />
+      ))}
+    </div>
+  )
 }
