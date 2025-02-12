3c7e60de1ac739d5331dfab694e1a397b586b6ef
Foursqweez <jeerasak.code@gmail.com>
2025-02-12 16:55:05 +0700

    Setup SQLite with Prisma

--- evento/src/lib/utils.ts
+++ evento/src/lib/utils.ts
@@ -1,6 +1,6 @@
+import { EventoEvent } from '@prisma/client'
 import clsx, { ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { EventoEvent } from './types'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
