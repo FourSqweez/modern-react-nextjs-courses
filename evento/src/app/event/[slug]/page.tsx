4aa024daf9f297f8ed0a22408ecec83289fb96a7
Foursqweez <jeerasak.code@gmail.com>
2025-02-12 12:46:33 +0700

    Try Client-sid cache (router cache)

--- evento/src/app/event/[slug]/page.tsx
+++ evento/src/app/event/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import H1 from '@/components/H1'
 import { EventoEvent } from '@/lib/types'
+import { sleep } from '@/lib/utils'
 import Image from 'next/image'
 
 type EventPageProps = {
@@ -10,6 +11,8 @@ type EventPageProps = {
 
 export default async function EventPage({ params }: EventPageProps) {
   const { slug } = params
+  await sleep(2000)
+
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`,
   )
