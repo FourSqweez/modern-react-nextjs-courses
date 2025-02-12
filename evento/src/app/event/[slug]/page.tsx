f079f02640a1b5af3070813a1c7d16d6975d81c5
Foursqweez <jeerasak.code@gmail.com>
2025-02-12 12:33:36 +0700

    Finish Event Page (Create reusable Section component)

--- evento/src/app/event/[slug]/page.tsx
+++ evento/src/app/event/[slug]/page.tsx
@@ -60,6 +60,34 @@ export default async function EventPage({ params }: EventPageProps) {
           </div>
         </div>
       </section>
+
+      <div className="min-h-[75vh} px-5 py-16 text-center">
+        <Section>
+          <SectionHeading>About this event</SectionHeading>
+          <SectionContent>{event.description}</SectionContent>
+        </Section>
+
+        <Section>
+          <SectionHeading>Location</SectionHeading>
+          <SectionContent>{event.location}</SectionContent>
+        </Section>
+      </div>
     </main>
+  )
+}
+
+function Section({ children }: { children: React.ReactNode }) {
+  return <section className="mb-12">{children}</section>
+}
+
+function SectionHeading({ children }: { children: React.ReactNode }) {
+  return <h2 className="mb-8 text-2xl">{children}</h2>
+}
+
+function SectionContent({ children }: { children: React.ReactNode }) {
+  return (
+    <p className="mx-auto max-w-4xl text-lg leading-8 text-white/75">
+      {children}
+    </p>
   )
 }
