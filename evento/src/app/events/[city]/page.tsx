a35704b66a381f2ba452a83e304e356356b2c237
Foursqweez <jeerasak.code@gmail.com>
2025-02-12 15:37:03 +0700

    Display event name as title (GenerateMetadata)

--- evento/src/app/events/[city]/page.tsx
+++ evento/src/app/events/[city]/page.tsx
@@ -1,23 +1,32 @@
 import EventsList from '@/components/EventsList'
 import H1 from '@/components/H1'
+import { capitalize } from '@/lib/utils'
+import { Metadata } from 'next'
 import { Suspense } from 'react'
 import LoadingCard from '../../../components/LoadingCard'
 
-type EventsPageProps = {
+type Props = {
   params: {
     city: string
   }
 }
 
-export default async function EventsPage({ params }: EventsPageProps) {
+export function generateMetadata({ params }: Props): Metadata {
+  const { city } = params
+
+  return {
+    title: city === 'all' ? 'All Events' : `Events in ${capitalize(city)}`,
+  }
+}
+
+export default async function EventsPage({ params }: Props) {
   const city = params.city
 
   return (
     <main className="flex min-h-[110vh] flex-col items-center px-[20px] py-24">
       <H1 className="mb-28">
         {city === 'all' && 'All Events'}
-        {city !== 'all' &&
-          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+        {city !== 'all' && `Events in ${capitalize(city)}`}
       </H1>
 
       <Suspense fallback={<LoadingCard />}>
