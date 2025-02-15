54af78e198bdcb7ed5409941fcbdc640a16d2b63
Foursqweez <jeerasak.code@gmail.com>
2025-02-15 19:03:22 +0700

    PetList styling and setup image constant

--- petsoft/src/components/pet-list.tsx
+++ petsoft/src/components/pet-list.tsx
@@ -1,8 +1,20 @@
+import { IMG_BASE_URL } from '@/lib/constant'
+import Image from 'next/image'
+
 export default function PetList() {
   return (
-    <ul className=" bg-white border-b border-black/[0.8]">
+    <ul className=" bg-white border-b border-black/[0.08]">
       <li>
-        <button>Benjamin</button>
+        <button className=" flex h-[70px] items-center w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] transition">
+          <Image
+            src={`${IMG_BASE_URL}/pet-placeholder.png`}
+            alt="Pet image"
+            width={45}
+            height={45}
+            className=" rounded-full object-cover"
+          />
+          <p className=" font-semibold">Benjamin</p>
+        </button>
       </li>
     </ul>
   )
---
+++ petsoft/src/lib/constant.ts
@@ -0,0 +1,1 @@
+export const IMG_BASE_URL = 'https://bytegrad.com/course-assets/react-nextjs'
