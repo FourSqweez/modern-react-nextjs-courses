a94e7501dd2d6c845f5993b29aebce35bd35759a
Foursqweez <jeerasak.code@gmail.com>
2025-02-15 19:28:07 +0700

    PetList fetch data

--- petsoft/src/components/pet-list.tsx
+++ petsoft/src/components/pet-list.tsx
@@ -1,21 +1,22 @@
-import { IMG_BASE_URL } from '@/lib/constant'
 import Image from 'next/image'
 
-export default function PetList() {
+export default async function PetList({ pets }) {
   return (
     <ul className=" bg-white border-b border-black/[0.08]">
-      <li>
-        <button className=" flex h-[70px] items-center w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] transition">
-          <Image
-            src={`${IMG_BASE_URL}/pet-placeholder.png`}
-            alt="Pet image"
-            width={45}
-            height={45}
-            className=" rounded-full object-cover"
-          />
-          <p className=" font-semibold">Benjamin</p>
-        </button>
-      </li>
+      {pets.map((pet: unknown) => (
+        <li key={pet.id}>
+          <button className=" flex h-[70px] items-center w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] transition">
+            <Image
+              src={pet.imageUrl}
+              alt="Pet image"
+              width={45}
+              height={45}
+              className="rounded-full object-cover w-[45px] h-[45px]"
+            />
+            <p className=" font-semibold">{pet.name}</p>
+          </button>
+        </li>
+      ))}
     </ul>
   )
 }
