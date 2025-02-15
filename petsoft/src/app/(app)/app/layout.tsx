def6cce65ba93c5cb7cd60749d9ca8194242c40a
Foursqweez <jeerasak.code@gmail.com>
2025-02-15 16:06:25 +0700

    Nested layout, Header navigation, App segment

---
+++ petsoft/src/app/(app)/app/layout.tsx
@@ -0,0 +1,21 @@
+import AppFooter from '@/components/app-footer'
+import AppHeader from '@/components/app-header'
+import BackgroundPattern from '@/components/background-pattern'
+import React from 'react'
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <BackgroundPattern />
+      <div className="max-w-[1050px] mx-auto px-4">
+        <AppHeader />
+        {children}
+        <AppFooter />
+      </div>
+    </>
+  )
+}
---
+++ petsoft/src/components/background-pattern.tsx
@@ -0,0 +1,3 @@
+export default function BackgroundPattern() {
+  return <div className="bg-[#2C9676] h-[300px] w-full absolute -z-10 top-0" />
+}
