78c0e70fa03d0de95ab078a890bdbd9bf2a9d515
Foursqweez <jeerasak.code@gmail.com>
2025-02-15 16:10:48 +0700

    App footer

--- petsoft/src/components/app-footer.tsx
+++ petsoft/src/components/app-footer.tsx
@@ -1,3 +1,9 @@
 export default function AppFooter() {
-  return <div>AppFooter</div>
+  return (
+    <footer className="mt-auto border-t border-black/5 py-5">
+      <small className="opacity-50">
+        &copy; Copyright by Four. Inspired by ByteGrad’s course.
+      </small>
+    </footer>
+  )
 }
