96c901e29f78366e06628ae08c2f1b4ff80b5c03
Foursqweez <jeerasak.code@gmail.com>
2025-02-17 13:21:05 +0700

    Reusable Dialog (Shadcn UI)

--- petsoft/src/components/pet-button.tsx
+++ petsoft/src/components/pet-button.tsx
@@ -1,6 +1,15 @@
 import { PlusIcon } from '@radix-ui/react-icons'
 import React from 'react'
+import PetForm from './pet-form'
 import { Button } from './ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog'
 
 type PetButtonProps = {
   actionType: 'add' | 'edit' | 'checkout'
@@ -12,18 +21,6 @@ export default function PetButton({
   children,
   onClick,
 }: PetButtonProps) {
-  if (actionType === 'add') {
-    return (
-      <Button size="icon">
-        <PlusIcon className="h-6 w-6" />
-      </Button>
-    )
-  }
-
-  if (actionType === 'edit') {
-    return <Button variant="secondary">{children}</Button>
-  }
-
   if (actionType === 'checkout') {
     return (
       <Button variant="secondary" onClick={onClick}>
@@ -31,4 +28,28 @@ export default function PetButton({
       </Button>
     )
   }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        {actionType === 'add' ? (
+          <Button size="icon">
+            <PlusIcon className="h-6 w-6" />
+          </Button>
+        ) : (
+          <Button variant="secondary">{children}</Button>
+        )}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {actionType === 'add' ? 'Add a new pet' : 'Edit pet'}
+          </DialogTitle>
+        </DialogHeader>
+
+        <PetForm />
+        <DialogFooter></DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
 }
