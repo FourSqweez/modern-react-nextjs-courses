7d8169d27b5a0f3824d2071d024fe7ccf033cfb2
Foursqweez <jeerasak.code@gmail.com>
2025-02-17 17:43:21 +0700

    Default image for form

--- petsoft/src/actions/actions.ts
+++ petsoft/src/actions/actions.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { IMG_BASE_URL } from '@/lib/constant'
 import { prisma } from '@/lib/prisma'
 
 export async function addPet(formData) {
@@ -8,7 +9,8 @@ export async function addPet(formData) {
       name: formData.get('name'),
       ownerName: formData.get('ownerName'),
       age: parseInt(formData.get('age')),
-      imageUrl: formData.get('imageUrl'),
+      imageUrl:
+        formData.get('imageUrl') || `${IMG_BASE_URL}/pet-placeholder.png`,
       notes: formData.get('notes'),
     },
   })
