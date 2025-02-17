6c1bb48d0f3f4fae9c20aeb97d4303fdeadf6b13
Foursqweez <jeerasak.code@gmail.com>
2025-02-17 19:44:42 +0700

    Server action refactoring - close dialog

--- petsoft/src/components/pet-form.tsx
+++ petsoft/src/components/pet-form.tsx
@@ -17,7 +17,13 @@ export default function PetForm({
   const { selectedPet } = usePetContext()
 
   return (
-    <form action={addPet} className="flex flex-col">
+    <form
+      action={(formData) => {
+        addPet(formData)
+        onFormSubmission()
+      }}
+      className="flex flex-col"
+    >
       <div className="space-y-3">
         <div className="space-y-1">
           <Label htmlFor="name">Name</Label>
