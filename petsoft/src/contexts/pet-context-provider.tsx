6fce6ac12bfe6d856d04ff002df4577fa77f7efd
Foursqweez <jeerasak.code@gmail.com>
2025-02-17 17:28:17 +0700

    AddPet server action

--- petsoft/src/contexts/pet-context-provider.tsx
+++ petsoft/src/contexts/pet-context-provider.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { addPet } from '@/actions/actions'
 import { Pet } from '@/lib/types'
 import React, { createContext, useState } from 'react'
 
@@ -37,14 +38,16 @@ export default function PetContextProvider({
     setSelectedPetId(null)
   }
 
-  const handleAddPet = (newPet: Omit<Pet, 'id'>) => {
-    setPets((prev) => [
-      ...prev,
-      {
-        ...newPet,
-        id: Date.now().toString(),
-      },
-    ])
+  const handleAddPet = async (newPet: Omit<Pet, 'id'>) => {
+    // setPets((prev) => [
+    //   ...prev,
+    //   {
+    //     ...newPet,
+    //     id: Date.now().toString(),
+    //   },
+    // ])
+
+    await addPet(newPet)
   }
 
   const handleEditPet = (petId: string, newPetData: Omit<Pet, 'id'>) => {
