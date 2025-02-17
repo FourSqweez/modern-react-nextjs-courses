457cbc1b45af9d3b6324f9bc633d0de0c492dd3b
Foursqweez <jeerasak.code@gmail.com>
2025-02-17 17:10:26 +0700

    Get data from database (Prisma client)

--- petsoft/src/app/(app)/app/layout.tsx
+++ petsoft/src/app/(app)/app/layout.tsx
@@ -3,7 +3,7 @@ import AppHeader from '@/components/app-header'
 import BackgroundPattern from '@/components/background-pattern'
 import PetContextProvider from '@/contexts/pet-context-provider'
 import SearchContextProvider from '@/contexts/search-context-provider'
-import { Pet } from '@/lib/types'
+import { prisma } from '@/lib/prisma'
 import React from 'react'
 
 type LayoutProps = {
@@ -11,21 +11,15 @@ type LayoutProps = {
 }
 
 export default async function Layout({ children }: LayoutProps) {
-  const response = await fetch(
-    'https://bytegrad.com/course-assets/projects/petsoft/api/pets'
-  )
-  if (!response.ok) {
-    throw new Error('Could not fetch pets')
-  }
+  const pets = await prisma.pet.findMany()
 
-  const data: Pet[] = await response.json()
   return (
     <>
       <BackgroundPattern />
-      <div className="flex flex-col max-w-[1050px] mx-auto px-4 min-h-screen">
+      <div className="mx-auto flex min-h-screen max-w-[1050px] flex-col px-4">
         <AppHeader />
         <SearchContextProvider>
-          <PetContextProvider data={data}>{children}</PetContextProvider>
+          <PetContextProvider data={pets}>{children}</PetContextProvider>
         </SearchContextProvider>
         <AppFooter />
       </div>
