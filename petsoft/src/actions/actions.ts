63433b479f17e702d0440aecd22bfb4bf640bac0
Foursqweez <jeerasak.code@gmail.com>
2025-02-19 13:15:33 +0700

    Server validation with Zod (Reuse Schema)

--- petsoft/src/actions/actions.ts
+++ petsoft/src/actions/actions.ts
@@ -3,13 +3,23 @@
 import { prisma } from '@/lib/prisma'
 import { OmitPetType, PetIdType } from '@/lib/types'
 import { sleep } from '@/lib/utils'
+import { petFormSchema } from '@/lib/validations'
 import { revalidatePath } from 'next/cache'
 
 export async function addPet(pet: OmitPetType) {
   await sleep(1000)
+
+  const validatedPet = petFormSchema.safeParse(pet)
+
+  if (!validatedPet.success) {
+    return {
+      message: 'invalid pet data.',
+    }
+  }
+
   try {
     await prisma.pet.create({
-      data: pet,
+      data: validatedPet.data,
     })
   } catch (error) {
     return {
--- petsoft/src/components/pet-form.tsx
+++ petsoft/src/components/pet-form.tsx
@@ -1,9 +1,9 @@
 'use client'
 import { DEFAULT_PET_IMAGE } from '@/lib/constant'
 import { usePetContext } from '@/lib/hooks'
+import { petFormSchema, TPetForm } from '@/lib/validations'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import { z } from 'zod'
 import PetFormBtn from './pet-form-btn'
 import { Input } from './ui/input'
 import { Label } from './ui/label'
@@ -13,28 +13,6 @@ type PetFormProps = {
   actionType: 'add' | 'edit'
   onFormSubmission: () => void
 }
-
-const petFormSchema = z
-  .object({
-    name: z.string().trim().min(1, { message: 'Name is required' }).max(100),
-    ownerName: z
-      .string()
-      .trim()
-      .min(1, { message: 'Owner name is required' })
-      .max(100),
-    imageUrl: z.union([
-      z.literal(''),
-      z.string().trim().url({ message: 'Image url must be a valid url' }),
-    ]),
-    age: z.coerce.number().int().positive().max(99999),
-    notes: z.union([z.literal(''), z.string().trim().max(1000)]),
-  })
-  .transform((data) => ({
-    ...data,
-    imageUrl: data.imageUrl || DEFAULT_PET_IMAGE,
-  }))
-
-type TPetForm = z.infer<typeof petFormSchema>
 
 export default function PetForm({
   actionType,
