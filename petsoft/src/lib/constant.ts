ed9604a8a5bd18aa46966a0c80d73fce90ac1123
Foursqweez <jeerasak.code@gmail.com>
2025-02-19 13:03:23 +0700

    Zod transform (Pet default image), cleanup: Fix Image

--- petsoft/src/lib/constant.ts
+++ petsoft/src/lib/constant.ts
@@ -1,1 +1,4 @@
 export const IMG_BASE_URL = 'https://bytegrad.com/course-assets/react-nextjs'
+
+export const DEFAULT_PET_IMAGE =
+  'https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png'
