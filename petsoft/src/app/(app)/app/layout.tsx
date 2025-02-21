83bb4e07a2426a559de5fa56cccdd17f4d8ac843
Foursqweez <jeerasak.code@gmail.com>
2025-02-21 15:48:19 +0700

    Get pets by user, change token type

--- petsoft/src/app/(app)/app/layout.tsx
+++ petsoft/src/app/(app)/app/layout.tsx
@@ -4,7 +4,9 @@ import BackgroundPattern from '@/components/background-pattern'
 import { Toaster } from '@/components/ui/sonner'
 import PetContextProvider from '@/contexts/pet-context-provider'
 import SearchContextProvider from '@/contexts/search-context-provider'
+import { auth } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 type LayoutProps = {
@@ -12,7 +14,17 @@ type LayoutProps = {
 }
 
 export default async function Layout({ children }: LayoutProps) {
-  const pets = await prisma.pet.findMany()
+  const session = await auth()
+
+  if (!session?.user) {
+    redirect('/login')
+  }
+
+  const pets = await prisma.pet.findMany({
+    where: {
+      userId: session.user.id,
+    },
+  })
 
   return (
     <>
