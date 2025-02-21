33bbf8596df4931a8ed1baa2cbd20da2046f5bfd
Foursqweez <jeerasak.code@gmail.com>
2025-02-21 18:14:10 +0700

    Fix typescript for pet form

--- petsoft/src/lib/types.ts
+++ petsoft/src/lib/types.ts
@@ -2,7 +2,7 @@ import { Pet, User } from '@prisma/client'
 
 export type PetType = Pet
 
-export type OmitPetType = Omit<Pet, 'id' | 'updatedAt' | 'createdAt'>
+export type OmitPetType = Omit<Pet, 'id' | 'updatedAt' | 'createdAt' | 'userId'>
 
 export type PetIdType = Pet['id']
 
