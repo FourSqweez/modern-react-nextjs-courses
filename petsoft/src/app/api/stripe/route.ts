7150098e82840626ea38f222cb8f406ce69ffb3d
Foursqweez <jeerasak.code@gmail.com>
2025-02-23 17:24:30 +0700

    Fulfill order (database update)

--- petsoft/src/app/api/stripe/route.ts
+++ petsoft/src/app/api/stripe/route.ts
@@ -1,7 +1,19 @@
+import { prisma } from '@/lib/prisma'
+
 export async function POST(request: Request) {
-  const body = await request.text()
+  const data = await request.json()
+
+  // verify webhook came from Stripe
 
   // fulfill order
+  await prisma.user.update({
+    where: {
+      email: data.data.object.customer_email,
+    },
+    data: {
+      hasAccess: true,
+    },
+  })
 
   // return 200 OK
   return Response.json(null, { status: 200 })
