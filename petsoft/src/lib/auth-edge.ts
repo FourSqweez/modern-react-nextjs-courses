6417a67a65adb21b41138eca90da4be17dcfa399
Foursqweez <jeerasak.code@gmail.com>
2025-02-24 15:10:22 +0700

    Separate bcrypt outside edge

--- petsoft/src/lib/auth-edge.ts
+++ petsoft/src/lib/auth-edge.ts
@@ -1,44 +1,10 @@
-import bcrypt from 'bcryptjs'
-import NextAuth, { NextAuthConfig } from 'next-auth'
-import Credentials from 'next-auth/providers/credentials'
-import { getUserByEmail } from './server-utils'
-import { authSchema } from './validations'
+import { NextAuthConfig } from 'next-auth'
+import { prisma } from './prisma'
 
-const config = {
+export const nextAuthEdgeConfig = {
   pages: {
     signIn: '/login',
   },
-  providers: [
-    Credentials({
-      async authorize(credentials) {
-        // runs on login
-        // validation
-        const validatedFormData = authSchema.safeParse(credentials)
-        if (!validatedFormData.success) {
-          return null
-        }
-
-        // extract values
-        const { email, password } = validatedFormData.data
-
-        const user = await getUserByEmail(email)
-        if (!user) {
-          console.log('No user found')
-          return null
-        }
-
-        const passwordsMatch = await bcrypt.compare(
-          password,
-          user.hashedPassword,
-        )
-        if (!passwordsMatch) {
-          console.log('Invalid credentials')
-          return null
-        }
-        return user
-      },
-    }),
-  ],
   callbacks: {
     authorized: ({ auth, request }) => {
       // runs on every request with middleware
@@ -93,7 +59,11 @@ const config = {
 
       if (trigger === 'update') {
         // on every request
-        const userFromDb = await getUserByEmail(token.email)
+        const userFromDb = await prisma.user.findUnique({
+          where: {
+            email: token.email,
+          },
+        })
         if (userFromDb) {
           token.hasAccess = userFromDb.hasAccess
         }
@@ -108,11 +78,5 @@ const config = {
       return session
     },
   },
+  providers: [],
 } satisfies NextAuthConfig
-
-export const {
-  auth,
-  signIn,
-  signOut,
-  handlers: { GET, POST },
-} = NextAuth(config)
